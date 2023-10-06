44660b454743186ebf9f96c824e7d203aad848e3
Rohith K P <rohithkp.me@gmail.com>
2023-10-06 17:22:00 +0530

    strapi bootstrap

---
+++ src/api/esop/routes/esop.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * esop router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::esop.esop');
---
+++ src/api/esop/services/esop.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * esop service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::esop.esop');
